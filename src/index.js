52d5adb03df8f587097f8f06c2fd146fe9efaeda
My Name <actualnicholas@gmail.com>
2022-10-19 19:14:18 -0700

    renamed things to be less stupid

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
+const ffmpeg = createFFmpeg({ log: true });
 
+let f = new FontFace('textile_regular', 'url("textile_regular.ttf") ')
+let can, canvas,img;
 
+//For updating the canvas whenever text is entered in the textarea.
+let input = document.getElementById('area');
+input.oninput = generateTitle;
+
+//Important since the FFMPEG module uses SharedArrayBuffer, and it's only activated if cross origin isolation is enabled.
 if(crossOriginIsolated){
     console.log('We are cors isolated')
 }
@@ -10,41 +18,30 @@ else{
 }
 
 
-const ffmpeg = createFFmpeg({ log: true });
-
-let f = new FontFace('test', 'url("textile_regular.ttf") ')
-let can, canvas, video, temp, c_temp,img;
-
-
-var value = document.getElementById('area');
-
-value.oninput = generateTitle;
-
-
-
-function generateTitle(text) {
+function generateTitle() {
    
-    console.log("input:",value.value);
-    console.log("array test::",value.value.split('\n'));
+    console.log("input:",input.value);
+    console.log("array test::",input.value.split('\n'));
 
-    let tits = value.value.split('\n');
+    let title = input.value.split('\n');
 
-    tits[0] = '"'+tits[0];
-    tits[tits.length-1] = tits[tits.length-1] + '"';
-    let k = [];
+// Adds the quotation marks to the first and last character.
+    title[0] = '"'+title[0];
+    title[title.length-1] = title[title.length-1] + '"';
+    
 
-  
+  //Repaints the canvas on input, otherwise the old text won't disappear and overlaps with the new text. 
     canvas.fillStyle = 'black';
     canvas.fillRect(0,0,1280,713);
     
-    for(let i=0 ; i<tits.length;i++){
+    for(let i=0 ; i<title.length;i++){
         canvas.fillStyle= 'white';
-        canvas.fillText(tits[i].split("").join(String.fromCharCode(8202)), 649, 345 + (72 * i));
+        canvas.fillText(title[i].split("").join(String.fromCharCode(8202)), 649, 345 + (72 * i));
         
-
     }
 
 }
+
 
 
 let button = document.getElementById('generate');
@@ -54,17 +51,41 @@ button.addEventListener('click',(event)=>{
 
     img = can.toDataURL('image/png');
     
-    Abed();
+    generateIntro();
 })
-async function Abed() {
-    console.log("Generating INtro");
+
+
+
+
+async function generateIntro() {
+    button.disabled = "true"
+    console.log("Generating Intro");
+
     let vidya = await fetch('./always sunny.mp4');
+    console.log("vidya",vidya);
+
+    if(!ffmpeg.isLoaded()){
+        await ffmpeg.load();
+    }
+
+    let userIntro = document.getElementById('output');
+    userIntro.style.display = "none"
 
     
-    console.log("vidya",vidya);
-    console.log("title",title);
+    let loadingAnim = document.getElementById('loading');
+    loadingAnim.innerText = "Loading";
+    
+    let loading = setInterval(()=>{
 
-    await ffmpeg.load();
+        if(loadingAnim.innerText.length < 10){
+            loadingAnim.innerText += "."
+        }
+        else{
+            loadingAnim.innerText = "Loading"
+        }
+            
+    },1000)
+    
     ffmpeg.FS('writeFile','always sunny.mp4', await fetchFile(vidya.url));
     ffmpeg.FS('writeFile','intro_title', await fetchFile(img));
     
@@ -74,16 +95,22 @@ async function Abed() {
 
     const url = URL.createObjectURL(new Blob([output.buffer],{type:'video/mp4'}));
 
-    let endMe = document.getElementById('output');
-    endMe.src = url;
-    endMe.style.display = "block";
+    clearInterval(loading);
+    loadingAnim.innerText = "";
 
+    button.disabled =!button.disabled;
 
-  
+    userIntro.src = url;
+    userIntro.style.display = "block";
+
+    
+ 
   }
 
 
 
+//Initializes the font so it can be used with Canvas.
+//Also initializes the Canvas.
 f.load().then((font) => {
 
     document.fonts.add(font);
@@ -92,14 +119,11 @@ f.load().then((font) => {
     can = document.getElementById('canvas');
     canvas = can.getContext('2d');
 
-    video = document.getElementById('video');
-
     canvas.fillStyle = 'black';
     canvas.fillRect(0,0,1280,713);
 
     canvas.fillStyle ='white';
-
-    canvas.font = '62px test';
+    canvas.font = '62px textile_regular';
     canvas.filter = "blur(0.7px)"
     canvas.textAlign = "center";
 
