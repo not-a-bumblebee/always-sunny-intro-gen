81cbe998ea6ecff10ff605ef2c74d8f0382d7f0a
My Name <actualnicholas@gmail.com>
2022-10-19 19:14:18 -0700

    renamed things to be less stupid

--- server.js
+++ server.js
@@ -3,21 +3,22 @@ const express = require('express');
 const PORT = 3000;
 let app = express();
 
-app.use((req, res, next)=> {
+app.use((req, res, next) => {
 
+  //To enable cross origin isolation.
   res.append("Cross-Origin-Embedder-Policy", "require-corp");
   res.append("Cross-Origin-Opener-Policy", "same-origin");
-  
-  
+
+
   next();
 });
 
 app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
-  
 
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
+  
 })
 
 
